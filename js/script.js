e246f75122cab450940cd7b80442360de88e4e4b
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-10 01:15:55 +0000

    add

--- js/script.js
+++ js/script.js
@@ -4,29 +4,46 @@
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
+;("use strict")
+
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit-5-05-JS/sw.js", {
+    scope: "/ICS2O-Unit-5-05-JS/",
   })
 }
 
-;("use strict")
 /**
- * This function calculates area of a parrallelogram.
+ * Input
  */
-function calculate() {
-  // input
-  const length = parseInt(document.getElementById("length-of-pyramid").value)
-  const width = parseInt(document.getElementById("width-of-pyramid").value)
-  const height = parseInt(document.getElementById("height-of-pyramid").value)
+function myButtonClicked() {
+  const counter1 = parseInt(document.getElementById("counter1").value);
+  const counter2 = parseInt(document.getElementById("counter2").value);
+  var addedNumber = 0;
+  var answer = 0;
 
-  // process
-  const volume = (length * width * height) / 3
-
-  // output
-  document.getElementById("answers").innerHTML =
-    "Volume is: " + volume.toFixed(2) + " cm³"
-}
+  if (counter1 > 0 && counter2 > 0) {
+    while (addedNumber < counter2) {
+      addedNumber = addedNumber + 1;
+      answer = answer + counter1;
+    }
+  } else if (counter1 < 0 && counter2 < 0) {
+    while (addedNumber > counter2) {
+      addedNumber = addedNumber - 1;
+      answer = answer - counter1;
+    }
+  } else if (counter1 > 0 && counter2 < 0) {
+    while (addedNumber > counter2) {
+      addedNumber = addedNumber - 1;
+      answer = answer - counter1;
+    }
+  } else if (counter1 < 0 && counter2 > 0) {
+    while (addedNumber < counter2) {
+      addedNumber = addedNumber + 1;
+      answer = answer + counter1;
+    }
+  }
+  document.getElementById("answer").innerHTML =
+    counter1 + " x " + addedNumber + " = " + answer;
